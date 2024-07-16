c12701fa94590f55b6e1e296ee22dc1d7cdc7a7f
Maia20-21 <maiabruna70@gmail.com>
2024-07-16 18:52:11 -0300

    while - caixa eletrônico

--- ex071.py
+++ ex071.py
@@ -1,9 +1,19 @@
-print('''----- CAIXA ELETRÔNICO -----
-• Possuímos cédulas de R$50, R$20, R$10 e R$1
-''')
+print(f'{" CAIXA ELETRÔNICO ":-^55}')
+print('• Possuímos cédulas de R$ 100, R$50, R$20, R$10 e R$1\n')
 
 valor = int(input('Qual valor você deseja sacar? R$'))
 total = valor
+cedulas = [100, 50, 20, 10, 1]
+resultado = {}
 
-while True:
-    
+for cedula in cedulas:
+    quantidade = total // cedula
+    if quantidade > 0:
+        resultado[cedula] = quantidade
+        total -= quantidade * cedula
+
+print(f'\n{" EXTRATO ":-^30}')
+for cedula, quantidade in resultado.items():
+    print(f'Cédulas de R${cedula}: {quantidade}')
+
+print(f'{" FIM DA TRANSAÇÃO ":-^30}')
