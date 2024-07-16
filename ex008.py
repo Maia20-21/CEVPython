16d3b330d584b9de871551819dcfec43a7ad24d0
Maia20-21 <maiabruna70@gmail.com>
2024-07-16 19:38:35 -0300

    dados e contas - conversor de medidas

--- ex008.py
+++ ex008.py
@@ -1,2 +1,4 @@
+print(f'{' CONVERSOR DE MEDIDAS ':-^40}')
+
 n1 = float(input('Digite um valor em metros: '))
-print(f'centímetros = {n1 * 100} \nmilímetros = {n1 * 1000}')
+print(f'centímetros = {n1 * 100} cm \nmilímetros = {n1 * 1000} mm')
