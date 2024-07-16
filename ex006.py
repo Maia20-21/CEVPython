9487fd966847ad01d664fc7b7329223fcc736fa8
Maia20-21 <maiabruna70@gmail.com>
2024-07-16 19:33:39 -0300

    dados e contas - dobro, triplo e raíz quadrada

--- ex006.py
+++ ex006.py
@@ -1,2 +1,4 @@
+print(f'{' DOBRO, TRIPLO E RAÍZ QUADRADA ':-^50}')
+
 n1 = int(input('Digite um valor: '))
-print(f'dobro = {n1 * 2} \ntriplo = {n1 * 3} \nraíz quadrada = {n1 ** 2}')
+print(f'\ndobro: {n1 * 2} \ntriplo: {n1 * 3} \nraíz quadrada: {n1 ** 2}')
