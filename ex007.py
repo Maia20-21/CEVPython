0f83fe28061c2c5282da8d1f5474145e7b9a2c3e
Maia20-21 <maiabruna70@gmail.com>
2024-07-16 19:35:41 -0300

    dados e contas - média aritmética

--- ex007.py
+++ ex007.py
@@ -1,4 +1,6 @@
-n1 = float(input('Nota 1 = '))
-n2 = float(input('Nota 2 = '))
+print(f'{' MÉDIA ARITMÉTICA ':-^30}')
 
-print(f'Média = {(n1+n2)/2}')
+n1 = float(input('Nota 1: '))
+n2 = float(input('Nota 2: '))
+
+print(f'Média: {(n1+n2)/2}')
